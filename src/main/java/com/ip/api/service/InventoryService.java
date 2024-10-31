9c6024a539d59c0fc4662b62f251875a921d2e8c
정교영 <rydud65@gmail.com>
2024-10-31 17:45:08 +0900

    feat: 제품등록,제품조회 API

--- src/main/java/com/ip/api/service/InventoryService.java
+++ src/main/java/com/ip/api/service/InventoryService.java
@@ -1,5 +1,20 @@
 package com.ip.api.service;
 
-public class InventoryService {
+import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+	
+	
+	// 제품 등록
+	// 등록된 제품 목록 ( 제품명 검색 기능, )
+	// 재고 리스트 ( 제품명, 유통기한 정렬, 제품명 검색기능 ) 
+	// 입고 ( 재고 등록 )
+	// 출고/소모
+	// 재고 조정
+	// 재고 이력 
+	// 재고 임계치 (재고수량이 제품별 임계 개수 이하가 되면 제품명 색 or 재고수량 색 변화) 
 }
