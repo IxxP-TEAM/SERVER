259bf8054a329d2b83d8c4076a0ce44377979b56
zoown12 <zoown12@gmail.com>
2024-11-11 11:18:18 +0900

    🐛 Fix: 주문 API 수정

--- src/main/java/com/ip/api/domain/Orders.java
+++ src/main/java/com/ip/api/domain/Orders.java
@@ -5,13 +5,11 @@ import com.ip.api.domain.enums.OrderStatus;
 import com.ip.api.domain.enums.PaymentMethod;
 import com.ip.api.domain.enums.PaymentStatus;
 import com.ip.api.domain.enums.ShippingStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import lombok.*;
 
@@ -38,7 +36,17 @@ public class Orders extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "userId")
     private User user;
+
     @ManyToOne
     @JoinColumn(name = "customer_id", referencedColumnName = "customerId")
     private Customer customer;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<OrderProduct> orderProducts = new ArrayList<>();
+
+    public void addOrderProduct(OrderProduct orderProduct) {
+        orderProducts.add(orderProduct);
+        orderProduct.setOrder(this); // OrderProduct의 order 필드 설정
+    }
 }
